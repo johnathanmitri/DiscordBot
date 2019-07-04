a4d2722660e2965c162cd8f64b783d6c5c63df63
Johnathan Mitri <johnathanmitri@gmail.com>
2019-07-04 16:50:03 -0700

    idk

--- bot.js
+++ bot.js
@@ -3,13 +3,20 @@ client = new Discord.Client();
 auth = require('./auth.json');
 
 client.on('ready', () => {
- console.log(`Logged in as ${client.user.tag}!`);
- });
+	console.log(`Logged in as ${client.user.tag}!`);
+});
 
 client.on('message', msg => {
- if (msg.content === 'ping') {
- msg.reply('pong');
- }
- });
+	Channel = msg.channel
+	if (Channel.guild != null)
+		console.log(`${msg.author.username} said "${msg.content}" in ${msg.channel.name} of ${msg.channel.guild.name}`)
+	else if (Channel.type == "dm")
+		console.log(`${msg.author.username} DMed "${msg.content}"`)
+	else
+		console.log('group chat?')
+
+	if (msg.content === 'ping') 
+		msg.reply('pong');
+});
 
 client.login(auth.token);
